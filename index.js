1110bca242b6c32987c788b4bcfbc95255816662
Masan-k <uronti@hotmail.co.jp>
2022-01-02 02:36:51 +0900

    モードボタンの判定範囲拡大＆選択時のカラー変更

--- index.js
+++ index.js
@@ -2,6 +2,29 @@
 
 let eCmbQuestion;
 
+function clickRadio(){
+  if(rdoEasy.checked){
+     lblEasy.style.background = "#FFF";
+     lblEasy.style.color  = "#000";
+  }else{
+     lblEasy.style.background = "#000";
+     lblEasy.style.color = "#FFF";
+  }
+  if(rdoNormal.checked){
+     lblNormal.style.background = "#FFF";
+     lblNormal.style.color  = "#000";
+  }else{
+     lblNormal.style.background = "#000";
+     lblNormal.style.color = "#FFF";
+  }
+  if(rdoHard.checked){
+     lblHard.style.background = "#FFF";
+     lblHard.style.color  = "#000";
+  }else{
+     lblHard.style.background = "#000";
+     lblHard.style.color = "#FFF";
+  }
+}
 function getSelectMode(){
     let eRdoMode = document.getElementsByName("rdoMode");
     for(let i=0;i<eRdoMode.length;i++){
@@ -225,105 +248,6 @@ function drawHiScore(){
     }	
 }
 
-function drawCtxLastYear() {
-    'use strict';
-
-    const X_BLANK_WIDTH = 60;
-    const Y_BLANK_WIDTH = 50;
-    const RECT_LENGTH = 10;
-    const BLANK_LENGTH = 3;
-    const INIT_BLANK_HEIGHT = 30;
-    const INIT_BLANK_WIDTH = 30;
-    const VERTICAL_COUNT = 7;
-    const HORIZONTAL_COUNT = 50;
-
-    let canvas = document.getElementById('cvsCatchUp');
-    let ctx = canvas.getContext('2d'); 
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    //Character drawing
-    ctx.lineWidth = '0.3';
-    ctx.strokeStyle = '#7A756D';
-    ctx.textBaseline = 'alphabetic';
-    ctx.strokeText('Mon', 5, 50.3);
-    ctx.strokeText('Wed', 5, 77.3);
-    ctx.strokeText('Fri', 5, 105.3);
-
-    ctx.textBaseline = 'top';
-    ctx.strokeText('Less', 500, 130);
-    ctx.fillStyle = getRectColor(0);
-    ctx.fillRect(528, 133, RECT_LENGTH, RECT_LENGTH)
-    
-    ctx.textBaseline = 'top';
-    ctx.fillStyle = getRectColor(2);
-    ctx.fillRect(540, 133, RECT_LENGTH, RECT_LENGTH)
-
-    ctx.fillStyle = getRectColor(4);
-    ctx.fillRect(552, 133, RECT_LENGTH, RECT_LENGTH)
-
-    ctx.fillStyle = getRectColor(6);
-    ctx.fillRect(564, 133, RECT_LENGTH, RECT_LENGTH)
-
-    ctx.fillStyle = getRectColor(8);
-    ctx.fillRect(576, 133, RECT_LENGTH, RECT_LENGTH)
-
-    ctx.fillStyle = '#7A756D';
-    ctx.strokeText('More', 595, 130);
-
-    //------------------
-    //Drawing of shapes
-    //------------------
-    ctx.lineWidth = '0.2';
-
-    let nowDate = new Date();
-    let dayOfWeek = nowDate.getDay();
-    let record = getPerDateCount();
-
-    let blockCount = (VERTICAL_COUNT * HORIZONTAL_COUNT) + dayOfWeek + 1;
-    let targetDate = new Date();
-    targetDate.setDate(nowDate.getDate() - (blockCount - 1));
-
-    let dateYmd = targetDate.getFullYear() + ('00' + (targetDate.getMonth()+1)).slice(-2) + ('00' + targetDate.getDate()).slice(-2);
-    let count = 0;
-
-    for(var x = 0; x <= HORIZONTAL_COUNT - 1; x++){
-        for(var y = 0; y <= VERTICAL_COUNT - 1; y++){
-                  
-            for (var key in Object.keys(record.dateYmd)){
-                if(dateYmd === record.dateYmd[key]){
-                    count = record.count[key];
-                }
-            }
-
-            ctx.fillStyle = getRectColor(count);
-            count = 0;
-            ctx.fillRect(INIT_BLANK_WIDTH + (BLANK_LENGTH + RECT_LENGTH) * x, INIT_BLANK_HEIGHT + (BLANK_LENGTH + RECT_LENGTH) * y, RECT_LENGTH, RECT_LENGTH);
-
-            
-            targetDate.setDate(targetDate.getDate() + 1);
-            dateYmd = targetDate.getFullYear() + ('00' + (targetDate.getMonth()+1)).slice(-2) + ('00' + targetDate.getDate()).slice(-2);
-        }
-    }
-    for(var y = 0; y <= dayOfWeek; y++){
-        for (var key in Object.keys(record.dateYmd)){
-            if(dateYmd === record.dateYmd[key]){
-                count = record.count[key];
-            }
-        }
-
-        ctx.fillStyle = getRectColor(count)
-        count = 0;
-        
-        ctx.fillRect(INIT_BLANK_WIDTH + (BLANK_LENGTH + RECT_LENGTH) * HORIZONTAL_COUNT, INIT_BLANK_HEIGHT + (BLANK_LENGTH + RECT_LENGTH) * y, RECT_LENGTH, RECT_LENGTH)
-        
-        targetDate.setDate(targetDate.getDate() + 1);
-        dateYmd = targetDate.getFullYear() + ('00' + (targetDate.getMonth()+1)).slice(-2) + ('00' + targetDate.getDate()).slice(-2);
-    }
-    ctx.stroke();
-}
-
-
 function init(contents) {
     'use strict';
 
@@ -374,7 +298,6 @@ function clickButton() {
 
 window.onload = function () {
     'use strict';
-    
 
     btnData.addEventListener("click", clickButton, false); 
 
@@ -403,6 +326,7 @@ window.onload = function () {
    }else{
         rdoEasy.checked = true;
    }
+   clickRadio();
    drawHiScore();
 
     let checkOption = document.getElementsByName('rdoMode');
@@ -412,5 +336,4 @@ window.onload = function () {
 	    drawHiScore();
         });
     });
-    drawCtxLastYear();
 };
