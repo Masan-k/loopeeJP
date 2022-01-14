7cab2988e1ec1b848762aabfbe7578fdb977cb81
Masan-k <uronti@hotmail.co.jp>
2022-01-14 14:55:48 +0900

    スマホでゲーム画面を開いた時に一部しか表示されない不具合を解消

--- tool/local_copy.sh
+++ tool/local_copy.sh
@@ -3,4 +3,5 @@ APP_NAME=japanese-geography
 sudo rm -r /var/www/my_domain/$APP_NAME/*
 sudo cp *.html /var/www/my_domain/$APP_NAME
 sudo cp *.js /var/www/my_domain/$APP_NAME
+sudo cp *.json /var/www/my_domain/$APP_NAME
 sudo cp -r img /var/www/my_domain/$APP_NAME
